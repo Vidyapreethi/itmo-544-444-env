d8f9941369ae9167e95a157cbbeb9812112abedb
Vidyapreethi <vparthas@hawk.iit.edu>
2015-10-04 18:17:25 -0500

    Added code to pull index.html from other git account

--- install-webserver.sh
+++ install-webserver.sh
@@ -1,9 +1,14 @@
+
 #!/bin/bash
 
 sudo apt-get update -y
 sudo apt-get install -y apache2
+git clone https://github.com/Vidyapreethi/itmo-544-444-fall2015.git
+mv itmo-544-444-fall2015/images /var/www/html/images
+mv itmo-544-444-fall2015/index.html /var/www/html
 
-echo "Hello!" > /tmp/hello.text
+
+echo "Hello!" > /tmp/hello.txt
 
 
 
