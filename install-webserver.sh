976b6983d521af9098fa9fb0484bd749dc12fede
Vidyapreethi <vparthas@hawk.iit.edu>
2015-10-04 18:50:12 -0500

    changed the path

--- install-webserver.sh
+++ install-webserver.sh
@@ -4,8 +4,8 @@
 sudo apt-get update -y
 sudo apt-get install -y apache2
 git clone https://github.com/Vidyapreethi/itmo-544-444-fall2015.git
-mv itmo-544-444-fall2015/images /var/www/html/images
-mv itmo-544-444-fall2015/index.html /var/www/html
+mv ./itmo-544-444-fall2015/images /var/www/html/images
+mv ./itmo-544-444-fall2015/index.html /var/www/html
 
 
 echo "Hello!" > /tmp/hello.txt
