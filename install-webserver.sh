18026242e5fb19c87a3a40f087d6819e65e01307
Vidyapreethi <vparthas@hawk.iit.edu>
2015-10-04 19:47:48 -0500

    Deleted initial blank line, removed move commands

--- install-webserver.sh
+++ install-webserver.sh
@@ -1,11 +1,7 @@
-
 #!/bin/bash
 
 sudo apt-get update -y
 sudo apt-get install -y apache2
-git clone https://github.com/Vidyapreethi/itmo-544-444-fall2015.git
-mv ./itmo-544-444-fall2015/images /var/www/html/images
-mv ./itmo-544-444-fall2015/index.html /var/www/html
 
 
 echo "Hello!" > /tmp/hello.txt
