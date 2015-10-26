337f45fa18b29817dda2335e1427da85f1e5b874
Vidyapreethi <vparthas@hawk.iit.edu>
2015-10-26 09:05:36 -0500

    added php5 and mysql-client

--- install-env.sh
+++ install-env.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-echo "Starting Server Setup" >> /tmp/webserver_install_log.txt
-echo "Working Directory is: " `pwd` >> /tmp/webserver_install_log.txt
 
 sudo apt-get update -y
-sudo apt-get install -y apache2
-
-echo "Installed apache2" >> /tmp/webserver_install_log.txt
+sudo apt-get install -y apache2 git php5-curl mysql-client 
 
 sudo apt-get install -y git
-
-echo "Installed Git" >> /tmp/webserver_install_log.txt
 
 git clone https://github.com/Vidyapreethi/itmo-544-444-fall2015.git
 
@@ -19,8 +13,7 @@ echo "Cloned Git Repo" >> /tmp/webserver_install_log.txt
 mv ./itmo-544-444-fall2015/images /var/www/html/images
 mv ./itmo-544-444-fall2015/index.html /var/www/html
 mv ./itmo-544-444-fall2015/page2.html /var/www/html
-
-echo "Copied images and pages to Apache directory" >> /tmp/webserver_install_log.txt
+mv ./itmo-544-444-fall2015/*.php /var/www/html
 
 echo "Hello!" > /tmp/hello.txt
 
