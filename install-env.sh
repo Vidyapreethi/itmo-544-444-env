9491ede0ccb26f017edd6624143baf7c3c0d1a30
Vidyapreethi <vparthas@hawk.iit.edu>
2015-11-08 17:04:42 -0600

    Adding instal.webserver.sh

--- install-env.sh
+++ install-env.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-
 sudo apt-get update -y
-sudo apt-get install -y apache2 git php5 mysql-client 
+sudo apt-get install -y apache2
+
 
 sudo apt-get install -y git
-
 git clone https://github.com/Vidyapreethi/itmo-544-444-fall2015.git
 
 echo "Cloned Git Repo" >> /tmp/webserver_install_log.txt
@@ -13,9 +12,5 @@ echo "Cloned Git Repo" >> /tmp/webserver_install_log.txt
 mv ./itmo-544-444-fall2015/images /var/www/html/images
 mv ./itmo-544-444-fall2015/index.html /var/www/html
 mv ./itmo-544-444-fall2015/page2.html /var/www/html
-mv ./itmo-544-444-fall2015/*.php /var/www/html
 
 echo "Hello!" > /tmp/hello.txt
-
-
-
---
+++ install-webserver.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+sudo apt-get update -y
+sudo apt-get install -y apache2 git php5 php5-curl mysql-client curl php5-mysql
+
+sudo apt-get install -y git
+git clone https://github.com/Vidyapreethi/itmo-544-444-fall2015.git
+
+
+echo "Cloned Git Repo" >> /tmp/webserver_install_log.txt
+
+mv ./itmo-544-444-fall2015/images /var/www/html/images
+mv ./itmo-544-444-fall2015/index.html /var/www/html
+mv ./itmo-544-444-fall2015/page2.html /var/www/html
+mv ./itmo-544-444-fall2015/*.php /var/www/html
+
+curl -sS https://getcomposer.org/installer | sudo php &> /tmp/getcomposer.txt
+
+sudo php composer.phar require aws/aws-sdk-php &> /tmp/runcomposer.txt
+
+sudo mv vendor /var/www/html &> /tmp/movevendor.txt
+
+sudo php /var/www/html/setup.php &> /tmp/database-setup.txt
+
+echo "Hello!" > /tmp/hello.txt
